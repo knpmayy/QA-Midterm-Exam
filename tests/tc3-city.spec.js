0ffa7108a9a0195f124bfb44b03e1d279898d4bc
LAPTOP-RVU2TVQO\knpmayy <kanokponbinhem@gmail.com>
2026-03-05 12:42:04 +0700

    setup tc3

---
+++ tests/tc3-city.spec.js
@@ -0,0 +1,13 @@
+//@ts-check
+import { test, expect } from '@playwright/test';
+
+test.describe('TC3: City field validation', () => {
+    test('TC3.1: city dropdown disabled until state selected', async ({ page }) => {
+        
+    });
+
+    test('TC3.2: city options filter by selected state', async ({ page }) => {
+
+    });
+
+});
