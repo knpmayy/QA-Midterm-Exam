5a4973d08c7d26cda194a56e8c0fa825a97735cb
LAPTOP-RVU2TVQO\knpmayy <kanokponbinhem@gmail.com>
2026-03-05 12:32:41 +0700

    setup tc1

---
+++ tests/tc1-submit.spec.js
@@ -0,0 +1,15 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+test.describe('TC1: Submit form with valid data', () => {
+    test('TC1.1: successfully submit form with all valid data in all fields', async ({ page }) => {
+        
+    });
+
+    test('TC1.2: successfully submit form with all valid data only in required fields', async ({ page }) => {
+        
+    });
+
+    test('TC1.3: modal correctly displays when submitted with valid data', async ({ page }) => {
+    
+    });
+});
