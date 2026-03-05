8607163e51e4b59de60da5d42821b148b37d3fdd
LAPTOP-RVU2TVQO\knpmayy <kanokponbinhem@gmail.com>
2026-03-05 12:45:14 +0700

    setup tc4

---
+++ tests/tc4-subjects.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test.describe('TC4: Subjects field validation', () => {
+    test('TC4.1: can add one subject', async ({ page }) => {
+    });
+
+    test('TC4.2: can add multiple subjects', async ({ page }) => {
+    });
+
+    test('TC4.3: can remove added subject', async ({ page }) => {
+    });
+    
+    test('TC4.4: cannot add non-existing subject', async ({ page }) => {
+
+    });
+
+});
